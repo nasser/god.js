c2e6250f95ef0b1c42cd721b13534b4aad6ec6ba
Ramsey Nasser <ram@nas.sr>
2013-03-02 04:15:35 -0500

    Wrap wrapper functions in object, make web accessible

--- 1.0_0/assets/js/wrapper.js
+++ 1.0_0/assets/js/wrapper.js
@@ -1,52 +1,52 @@
+var Wrapper = {
+	has_on_page: function(_str) {
+		var retval = false;
+		$("div").each(function(index) { if($(this).text().search(_str) > -1) { retval = true;} })
+		return retval;
+	},
 
-
-function has_on_page(_str) {
-	var retval = false;
-	$("div").each(function(index) { if($(this).text().search(_str) > -1) { retval = true;} })
-	return retval;
-}
-
-function is_on_domain(domain) {
-	if(window.location.hostname == domain || window.location.hostname == domain+".com" || window.location.hostname == "www."+domain+".com") {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function has_in_domain(domain) {
-	if(window.location.hostname.search(domain) > -1) {
-		return true;
-	} else {
-		return false;
-	}
-}
-
-function speak_this(msg) {
-	chrome.tts.speak(msg);
-}
-
-function remove_random_bookmark() {
-	chrome.bookmarks.search("a", function(results) {
-		b = results[Math.floor(Math.random() * results.length)];
-		alert("FAKE REMOVING "+b.title);
-	//	chrome.bookmarks.remove(b.id);
-	});
-}
-
-function add_bookmark(name, url) {
-	chrome.bookmarks.create({'title': name, 'url': url}, function done(b) {
-	});
-}
-
-function close_tabs(q) {
-	chrome.tabs.query(q, function done(tabs) {
-		for (var i=0; i<tabs.length; i++) {
-			chrome.tabs.remove(tabs[i].id);
+	is_on_domain: function(domain) {
+		if(window.location.hostname == domain || window.location.hostname == domain+".com" || window.location.hostname == "www."+domain+".com") {
+			return true;
+		} else {
+			return false;
 		}
-	});
-}
+	},
 
-function create_tab(url) {
-	chrome.tabs.create({'url': url});	
+	has_in_domain: function(domain) {
+		if(window.location.hostname.search(domain) > -1) {
+			return true;
+		} else {
+			return false;
+		}
+	},
+
+	speak_this: function(msg) {
+		chrome.tts.speak(msg);
+	},
+
+	remove_random_bookmark: function() {
+		chrome.bookmarks.search("a", function(results) {
+			b = results[Math.floor(Math.random() * results.length)];
+			alert("FAKE REMOVING "+b.title);
+		//	chrome.bookmarks.remove(b.id);
+		});
+	},
+
+	add_bookmark: function(name, url) {
+		chrome.bookmarks.create({'title': name, 'url': url}, function done(b) {
+		});
+	},
+
+	close_tabs: function(q) {
+		chrome.tabs.query(q, function done(tabs) {
+			for (var i=0; i<tabs.length; i++) {
+				chrome.tabs.remove(tabs[i].id);
+			}
+		});
+	},
+
+	create_tab: function(url) {
+		chrome.tabs.create({'url': url});	
+	}
 }
